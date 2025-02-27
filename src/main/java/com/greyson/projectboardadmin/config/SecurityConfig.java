4629c47330577d1771c39e1e14cd36a0759a896e
dino-park <easternshy@gmail.com>
2025-02-27 16:38:25 +0900

    #14 - 코드 리팩토링

--- src/main/java/com/greyson/projectboardadmin/config/SecurityConfig.java
+++ src/main/java/com/greyson/projectboardadmin/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class SecurityConfig {
     @Bean
@@ -14,6 +16,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .formLogin(Customizer.withDefaults())
                 .logout(logout -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(withDefaults())
                 .build();
     }
 }

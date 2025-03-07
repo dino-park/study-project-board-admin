b431539bbbefe39159f62ab53868cc3c1f7f624b
dino-park <easternshy@gmail.com>
2025-03-07 18:07:37 +0900

    #16 - 어드민 회원 테이블명 변경
    `user_account` -> `admin_account` 로 어드민 회원 테이블명 변경

---
+++ src/main/java/com/greyson/projectboardadmin/dto/AdminAccountDto.java
@@ -0,0 +1,60 @@
+package com.greyson.projectboardadmin.dto;
+
+import com.greyson.projectboardadmin.domain.AdminAccount;
+import com.greyson.projectboardadmin.domain.constant.RoleType;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+/**
+ * DTO for {@link AdminAccount}
+ */
+public record AdminAccountDto(
+        String userId,
+        String userPassword,
+        Set<RoleType> roleTypes,
+        String email,
+        String nickname,
+        String memo,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy) {
+
+    public static AdminAccountDto of(String userId, String userPassword, Set<RoleType> roleTypes,
+                                     String email, String nickname, String memo) {
+        return AdminAccountDto.of(userId, userPassword, roleTypes, email, nickname, memo, null, null, null, null);
+    }
+
+    public static AdminAccountDto of(String userId, String userPassword, Set<RoleType> roleTypes,
+                                     String email, String nickname, String memo, LocalDateTime createdAt, LocalDateTime modifiedAt,
+                                     String createdBy, String modifiedBy) {
+        return new AdminAccountDto(userId, userPassword, roleTypes, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static AdminAccountDto from(AdminAccount entity) {
+        return new AdminAccountDto(
+                entity.getUserId(),
+                entity.getUserPassword(),
+                entity.getRoleTypes(),
+                entity.getEmail(),
+                entity.getNickname(),
+                entity.getMemo(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public AdminAccount toEntity() {
+        return AdminAccount.of(
+                userId,
+                userPassword,
+                roleTypes,
+                email,
+                nickname,
+                memo
+        );
+    }
+}

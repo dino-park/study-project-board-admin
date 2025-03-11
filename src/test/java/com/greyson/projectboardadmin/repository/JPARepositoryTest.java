d6a16fa96b110e7d64fa8e953da91aa6b03dbd11
dino-park <easternshy@gmail.com>
2025-03-11 14:48:33 +0900

    #16 - 회원 관리 비즈니스 로직 및 컨트롤러 테스트 작성

--- src/test/java/com/greyson/projectboardadmin/repository/JPARepositoryTest.java
+++ src/test/java/com/greyson/projectboardadmin/repository/JPARepositoryTest.java
@@ -31,7 +31,7 @@ class JPARepositoryTest {
 
     @DisplayName("회원 정보 select 테스트")
     @Test
-    void givenUserAccounts_whenSelecting_thenWorksFine() {
+    void givenAdminAccounts_whenSelecting_thenWorksFine() {
         // Given
 
         // When
@@ -45,7 +45,7 @@ class JPARepositoryTest {
 
     @DisplayName("회원 정보 insert 테스트")
     @Test
-    void givenUserAccounts_whenInserting_thenWorksFine() {
+    void givenAdminAccounts_whenInserting_thenWorksFine() {
         // Given
         long previousCount = adminAccountRepository.count();
         AdminAccount adminAccount = AdminAccount.of("test", "pw", Set.of(RoleType.DEVELOPER), null, null, null);
@@ -58,7 +58,7 @@ class JPARepositoryTest {
 
     @DisplayName("회원 정보 update 테스트")
     @Test
-    void givenUserAccountAndRoleType_whenUpdating_thenWorksFine() {
+    void givenAdminAccountAndRoleType_whenUpdating_thenWorksFine() {
         // Given
         AdminAccount adminAccount = adminAccountRepository.getReferenceById("uno");
         adminAccount.addRoleType(RoleType.DEVELOPER);
@@ -75,7 +75,7 @@ class JPARepositoryTest {
 
     @DisplayName("회원 정보 delete 테스트")
     @Test
-    void givenUserAccount_whenDeleting_thenWorksFine() {
+    void givenAdminAccount_whenDeleting_thenWorksFine() {
         // Given
         long previousCount = adminAccountRepository.count();
         AdminAccount adminAccount = adminAccountRepository.getReferenceById("uno");
